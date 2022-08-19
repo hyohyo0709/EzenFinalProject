e0164b81d727d7453ce8b148766aa90a024ea883
KDH <recries12@gamil.com>
2022-08-19 12:38:13 +0900

    20220819|pull용

--- src/main/java/ezenproject/dao/OrderDAO.java
+++ src/main/java/ezenproject/dao/OrderDAO.java
@@ -24,6 +24,7 @@ public interface OrderDAO {
 	public List<OrderDTO> myOrderList(String memnumber);
 	public OrderDTO selectOrderInformation(int num);
 	public OrderDTO checkOrderStatus(String order_number);
+	public OrderDTO checkOrderStatus(int num);
 	
 	public void updateOrderInformation(OrderDTO dto);
 }
